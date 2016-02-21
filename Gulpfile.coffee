4742bf9bbca5110783ea14bc224bcc782b6af1a4
Nathan Ringo <remexre@gmail.com>
2016-02-21 13:57:26 -0600

    Tabs to spaces, 's to "s.

--- Gulpfile.coffee
+++ Gulpfile.coffee
@@ -4,24 +4,24 @@ jade = require "gulp-jade"
 sass = require "gulp-sass"
 
 targets = [
-  "coffee"
-  "jade"
-  "sass"
+	"coffee"
+	"jade"
+	"sass"
 ]
 
 gulp.task "default", targets
 
 gulp.task "coffee", ->
-  gulp.src "src/coffee/*.coffee"
-    .pipe coffee()
-    .pipe gulp.dest "launch_physics/static/js/"
+	gulp.src "src/coffee/*.coffee"
+		.pipe coffee()
+		.pipe gulp.dest "launch_physics/static/js/"
 
 gulp.task "jade", ->
-  gulp.src "src/jade/*.jade"
-    .pipe jade {}
-    .pipe gulp.dest "launch_physics/templates/"
+	gulp.src "src/jade/*.jade"
+		.pipe jade {}
+		.pipe gulp.dest "launch_physics/templates/"
 
 gulp.task "sass", ->
-  gulp.src "src/sass/*.scss"
-    .pipe sass().on "error", sass.logError
-    .pipe gulp.dest "launch_physics/static/css/"
+	gulp.src "src/sass/*.scss"
+		.pipe sass().on "error", sass.logError
+		.pipe gulp.dest "launch_physics/static/css/"
--- launch_physics/views.py
+++ launch_physics/views.py
@@ -1,45 +1,47 @@
 from flask import make_response, render_template, request
 from launch_physics import app
+from launch_physics.login import login
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def index_get():
+	print(request.cookies)
+	return render_template("index.html")
 
-@app.route('/account')
-def account():
-    return render_template('account.html')
+@app.route("/account")
+def account_get():
+	return render_template("account.html")
 
-@app.route('/badges')
-def badges():
-    return render_template('badges.html')
+@app.route("/badges")
+def badges_get():
+	return render_template("badges.html")
 
-@app.route('/login', methods=['GET'])
-def login():
-    return render_template('login.html')
+@app.route("/login", methods=["GET"])
+def login_get():
+	return render_template("login.html")
 
-@app.route('/login', methods=['POST'])
-def do_login():
-    username = request.form['username']
-    password = request.form['password']
-    return 'TODO: ' + username + ':' + password
+@app.route("/login", methods=["POST"])
+def login_post():
+	username = request.form["username"]
+	password = request.form["password"]
+	return "TODO: " + login(username, password)
 
-@app.route('/logout')
-def logout():
-    resp = make_response(render_template('redirect.html', url='/'))
-    resp.headers['Location'] = '/'
-    resp.status_code = 303
-    resp.set_cookie('auth', value='', expires=0)
-    return resp
+@app.route("/logout")
+def logout_get():
+	resp = make_response(render_template("redirect.html", url="/"))
+	resp.headers["Location"] = "/"
+	resp.status_code = 303
+	resp.set_cookie("auth", value="", expires=0)
+	return resp
 
-@app.route('/register', methods=['GET'])
-def register():
-    return render_template('register.html')
+@app.route("/register", methods=["GET"])
+def register_get():
+	return render_template("register.html")
 
-@app.route('/topic/<topic>')
-def topic(topic):
-    modules = ['example_module', 'another_module', 'a_third_module']
-    return render_template('topic.html', modules=modules, topic=topic)
+@app.route("/topic/<topic>")
+def topic_view(topic):
+	modules = ["example_module", "another_module", "a_third_module"]
+	return render_template("topic.html", modules=modules, topic=topic)
 
-@app.route('/topic/<topic>/<module>')
-def module(module, topic):
-    return render_template('module.html', module=module, topic=topic)
+@app.route("/topic/<topic>/<module>")
+def module_view(module, topic):
+	return render_template("module.html", module=module, topic=topic)
